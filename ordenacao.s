32cb8f699c92a2cfbc1faf6b5c808b8b7ccbce71
Ubuntu <ubuntu@ip-172-31-34-11.ec2.internal>
2023-07-06 19:33:11 +0000

    tudo errado

--- ordenacao.s
+++ ordenacao.s
@@ -21,27 +21,7 @@ main:
         movq [rdx + 8], 5
         movq [rdx + 12], 2
         movq [rdx + 16], 4
-    ordenar_vetor:
-        for_i:
-            mov rcx, 0 # i = 0
-            cmp rcx, 4
-            je print_vector_init
-            mov [rbp - 4], rcx # min = i
-            for_j:
-                mov r8, [rcx + 1] # j = i + 1
-                cmp r8, 5
-                je cont_for_i
-                mov rdx, [rip + vector] 
-                mov rdx, [rdx + r8 * 4] # rdx = V[j]
-                mov rdi, [rip + vector] 
-                mov r9, [rbp - 4] # r9 = min
-                cmp rdx, [rdi + r9 * 4]
-                jl min_j
-                min_j:
-                    mov [rbp - 4], 
-        cont_for_i:
-            
-                
+        call ordenar_vetor
     print_vector_init:
         mov rcx, 0
 	print_vector:
@@ -65,6 +45,45 @@ done:
     mov rsp, rbp
     pop rbp
     ret
+
+ordenar_vetor:
+    for_i_init:
+        mov rcx, 0 # i = 0
+    for_i:
+        cmp rcx, 4
+        je print_vector_init
+        mov [rbp - 4], rcx # min = i
+        for_j_init:
+            mov r8, [rcx + 1] # j = i + 1
+        for_j:
+            cmp r8, 5
+            je cont_for_i
+            mov rdx, [rip + vector] 
+            mov rdx, [rdx + r8 * 4] # rdx = V[j]
+            mov rdi, [rip + vector] 
+            mov r9, [rbp - 4] # r9 = min
+            cmp rdx, [rdi + r9 * 4] # comparacao V[j] e V[min]
+            jl min_menor_que_j
+            jmp for_j
+    cont_for_i:
+        cmp rcx, [rbp - 4]
+        jne swap
+        inc rcx
+        jmp for_i
+    ret
+
+min_menor_que_j:
+    mov [rbp - 4], r8
+    jmp for_j
+    
+swap:
+    mov rdx, [rip + vector]
+    mov r9, [rdx + rcx * 4] # V[i]
+    mov r10, [rdx + 
+    mov [rcx, [rbp - 4]
+    mov [rbp - 4], rcx
+    inc rcx
+    jmp for_i
     
 .section .bss
     vector: .8byte  
@@ -72,4 +91,5 @@ done:
 .section .rodata
     print_v: .string "%u "
     final: .string "\n"
+    
     
